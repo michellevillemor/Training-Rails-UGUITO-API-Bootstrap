ed6ddf8b833c0690200fc8b40c467eb47b77a5d4
michellevillemor <michelle.devillemor@widergy.com>
2024-05-10 16:36:10 -0300

    style: minor fixes in style

--- app/models/note.rb
+++ app/models/note.rb
@@ -16,22 +16,26 @@ class Note < ApplicationRecord
   validate :validate_content_length
 
   enum note_type: { review: 0, critique: 1 }
-  
+
   belongs_to :user
   has_one :utility, through: :user
 
-  SHORT_THRESHOLD = { 'North Utility' => 50, 'South Utility' => 60 }
-  MEDIUM_THRESHOLD = { 'North Utility' => 100, 'South Utility' => 120 }
+  SHORT_THRESHOLD = { 'North Utility' => 50, 'South Utility' => 60 }.freeze
+  MEDIUM_THRESHOLD = { 'North Utility' => 100, 'South Utility' => 120 }.freeze
 
   def validate_content_length
-    if utility.present? && content_length != 'short' && note_type == 'review'
-      error_message = I18n.interpolate("%{note_type} %{content_length}", 
-                                      note_type: I18n.t("activerecord.models.note.note_type.#{note_type}"),
-                                      content_length: I18n.t('activerecord.errors.note.content_length')
-                                    ) + " #{SHORT_THRESHOLD[utility.name]} palabras"
+    return unless content_length != 'short' && note_type == 'review'
 
-      errors.add(:content, error_message)
-    end
+    note_type_translation = I18n.t("activerecord.models.note.note_type.#{note_type}")
+    content_length_translation = I18n.t('activerecord.errors.note.content_length')
+
+    error_message = I18n.interpolate('%<note_type>s %<content_length>s %<threshold>s %<words>s',
+                                     note_type: note_type_translation,
+                                     content_length: content_length_translation,
+                                     threshold: SHORT_THRESHOLD[utility.name],
+                                     words: I18n.t('words'))
+
+    errors.add(:content, error_message)
   end
 
   def word_count
@@ -42,13 +46,12 @@ class Note < ApplicationRecord
     short_threshold = SHORT_THRESHOLD[utility.name]
     medium_threshold = MEDIUM_THRESHOLD[utility.name]
 
-    case self.word_count
-      when 0..short_threshold
-        'short'
-      when short_threshold + 1..medium_threshold
-        'medium'
-      else 'long'
+    case word_count
+    when 0..short_threshold
+      'short'
+    when short_threshold + 1..medium_threshold
+      'medium'
+    else 'long'
     end
   end
-
 end
